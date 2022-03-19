7d6796559e98cdb7764265cab4dc0f0eb58c5b06
kharcheamit <94366181+kharcheamit@users.noreply.github.com>
2022-03-19 13:53:40 +0530

    Add files via upload

---
+++ HomePageTest.java
@@ -0,0 +1,11 @@
+public class HomePageTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        HomePage homePage = new HomePage();
+
+        System.out.println("open web page");
+
+    homePage.waits();
+        System.out.println("close web Page");
+    }
+}
